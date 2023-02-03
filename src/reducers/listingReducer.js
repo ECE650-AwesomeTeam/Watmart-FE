8d1b348759517fe86e2ace21920246fee86faf49
Quan <qqmquach@uwtaerloo.ca>
2023-02-03 13:55:06 -0500

    add reducers folder

---
+++ src/reducers/listingReducer.js
@@ -0,0 +1,11 @@
+import { ActionTypes } from "../api/types";
+// const listingReducer = (
+//     state: ......
+//     action: ......
+// ) => {
+//     switch(action.type) {
+//         case ...:
+//             return { ... state, data: action.payload };
+//     }
+// };
+// export default listingReducer;
