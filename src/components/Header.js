b36d22b7b47cefd02530f85f62d0ec954127e464
Quan <qqmquach@uwtaerloo.ca>
2023-02-06 18:15:36 -0500

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import '../css/Header.css';
+import {HiMenu} from 'react-icons/hi';
+import {FaRegTimesCircle} from 'react-icons/fa';
+import {BsFillHouseFill} from 'react-icons/bs';
+
+const Header = () => {
+
+    const  [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    return (
+        <div className="header">
+            <div className="container">
+                <h1><span><BsFillHouseFill />Real</span>Estate</h1>
+                <button className="btn">Sign in</button>
+                <ul className={ click ? 'nav-menu active' : 'nav-menu' }>  
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Search</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div className="hamburger" onClick={handleClick}>
+                    { click ? (<FaRegTimesCircle className="icon" />) : (<HiMenu className="icon"/>)}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
