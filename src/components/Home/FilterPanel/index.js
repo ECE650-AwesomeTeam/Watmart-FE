b0c05d59a9c015a968bc9abf8ec523093be134ee
DelinM <delinmuray@gmail.com>
2023-02-20 23:12:22 -0500

    ToggleUpdates

--- src/components/Home/FilterPanel/index.js
+++ src/components/Home/FilterPanel/index.js
@@ -1,11 +1,51 @@
 import React from 'react'
-
-const List = () => {
-    return (
-        <div>
-            <h1>Filter Panel</h1>
+import CheckboxProton from "./CheckboxProton";
+import FilterListToggle from "./FilterListToggle";
+import SliderProton from "./SliderProton";
+import { categoryList, ratingList } from './filterFake';
+import './styles.css';
+const FilterPanel = ({
+                         selectedCategory,
+                         selectCategory,
+                         selectedRating,
+                         selectedPrice,
+                         selectRating,
+                         cuisines,
+                         changeChecked,
+                         changePrice,
+                     }) => (
+    <div>
+        <div className='input-group'>
+            <p className='label'>Category</p>
+            <FilterListToggle
+                options={categoryList}
+                value={selectedCategory}
+                selectToggle={selectCategory}
+            />
         </div>
-    );
-};
+        <div className='input-group'>
+            <p className='label'>Cuisine</p>
+            {cuisines.map((cuisine) => (
+                <CheckboxProton
+                    key={cuisine.id}
+                    cuisine={cuisine}
+                    changeChecked={changeChecked}
+                />
+            ))}
+        </div>
+        <div className='input-group'>
+            <p className='label-range'>Price Range</p>
+            <SliderProton value={selectedPrice} changePrice={changePrice} />
+        </div>
+        <div className='input-group'>
+            <p className='label'>Star Rating</p>
+            <FilterListToggle
+                options={ratingList}
+                value={selectedRating}
+                selectToggle={selectRating}
+            />
+        </div>
+    </div>
+);
 
-export default List
+export default FilterPanel;
