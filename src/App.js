bc0a54fa6bfb3677b28d7b07a07e4537adfd726b
Delin Mu <87948902+DelinM@users.noreply.github.com>
2023-02-22 17:33:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
+import WTRoutes from "./components/WTRoutes";
 
-// import Listings from "./components/Listings";
-import Filter from "./components/Filter"
-// import Signup from "./components/Signup";
-// import Login from "./components/Login"
-function App() {
-  return (
-    <>
-      <Filter />
-    </>
-  );
+const App: React.FC<{}> = () => {
+    return (
+        <WTRoutes />   
+    );
 }
 
 export default App;
