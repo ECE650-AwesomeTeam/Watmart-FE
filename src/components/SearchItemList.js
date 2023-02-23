8b9996bf4f85d479e606ff4159edfb16e6d3090b
Quan <qqmquach@uwtaerloo.ca>
2023-02-22 21:18:53 -0500

    update search page with filter component

--- src/components/SearchItemList.js
+++ src/components/SearchItemList.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from './Header';
+import styles from '../css/SearchItemList.module.css';
+import Filter from "./Filter";
 
 const SearchItemList = props => {
     return (
-        <>
+        <div className={styles.container}>
             <Header />
-            {/*your code goes from here */}
-        </>
+            <div className={styles["content-container"]}>
+                <div className={styles["filter-container"]}>
+                    <div className={styles.filter}>
+                        <Filter />
+                    </div>
+                </div>
+                <div className={styles["items-container"]}>
+                </div>
+            </div>
+        </div>
     )
 };
 
