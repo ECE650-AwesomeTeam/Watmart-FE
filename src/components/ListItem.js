d42d72c10ce5b8606e9a54a19cf55f61f6fa39a8
Quan <qqmquach@uwtaerloo.ca>
2023-03-25 17:55:00 -0400

    add link to read more button

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,8 +4,12 @@ import styles from '../css/ListItem.module.css';
 import FormatPrice from "./FormatPrice";
 
 const ListItem = ({ desc, img, price, name }) => {
+    const navigate = useNavigate();
+    const listingClickHandler = () => {
+        navigate("/listing/300");
+    };
     return (
-        <div className={`${styles.card} ${styles.grid} ${styles["grid-two-column"]}`}>
+        <div className={`${styles.card} ${styles.grid} ${styles["grid-two-column"]}`} onClick={listingClickHandler}>
             <figure className={styles.figure}>
                 <img className={styles.img} src={img} alt={name} />
             </figure>
