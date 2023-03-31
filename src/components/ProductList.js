2590b0c804321137095952e9c3a3747aae490526
Quan <qqmquach@uwtaerloo.ca>
2023-03-30 23:41:29 -0400

    correct issue

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,13 +4,13 @@ import { useFilterContext  } from "../contexts/FilterContext";
 import AdsList from "./AdsList";
 
 const ProductList = ({ category }) => {
-    const { filter_products, grid_view } = useFilterContext();
+    const { filter_products, grid_view, all_products } = useFilterContext();
     console.log("category =" + category);
     if (grid_view === true) {
-        return <AdsList numCol={4} products={ category !== "all" ? filter_products.filter(item => {return item.category.toLowerCase() === category; }) : filter_products }/>
+        return <AdsList numCol={4} products={ category !== "all" ? all_products.filter(item => {return item.category.toLowerCase() === category; }) : filter_products }/>
     }
     if (grid_view === false) {
-        return <ListView products={category !== "all" ? filter_products.filter(item => {return item.category.toLowerCase() === category; }) : filter_products}/>
+        return <ListView products={category !== "all" ? all_products.filter(item => {return item.category.toLowerCase() === category; }) : filter_products}/>
     }
 };
 
